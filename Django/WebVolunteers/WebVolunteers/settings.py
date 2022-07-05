d747f63be2c943a5dac74f68213ae64cb6668528
Михаил Маслов <mmet10182@yandex.ru>
2022-07-05 17:02:16 +0300

    Fix ALLOWED_HOSTS

--- Django/WebVolunteers/WebVolunteers/settings.py
+++ Django/WebVolunteers/WebVolunteers/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS")
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+print(ALLOWED_HOSTS)
 
 # Application definition
 
